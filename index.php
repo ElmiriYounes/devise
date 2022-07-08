cd539add262c30dbc9b4cd152e8beae254c6edef
elmiri younes <elmiri.younes@hotmail.com>
2022-07-08 15:54:31 +0200

    devises

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-include_once('variables.php');
-session_start();
-if (isset($_SESSION['clicked'])) {
-    // echo $_SESSION['clicked'];
+include_once 'variables.php';
+include_once 'functions.php';
+
+if (isset($_POST['switched'])) {
+    $temp = $_POST['devise1'];
+    $_POST['devise1'] = $_POST['devise2'];
+    $_POST['devise2'] = $temp;
 }
 ?>
 <!DOCTYPE html>
@@ -19,6 +22,8 @@ if (isset($_SESSION['clicked'])) {
     <style>
     * {
         box-sizing: border-box;
+        padding: 0;
+        margin: 0;
     }
 
     a {
@@ -28,52 +33,70 @@ if (isset($_SESSION['clicked'])) {
 
     body {
         width: 100%;
-        height: 100vh;
+        min-height: 100vh;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        color: orangered;
-        overflow: hidden;
-        background-color: black;
+        color: white;
+        background: url('bg.png') no-repeat;
+        background-position: center;
+        background-size: cover;
     }
 
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
-        /* display: none; <- Crashes Chrome on hover */
         -webkit-appearance: none;
         margin: 0;
-        /* <-- Apparently some margin are still there even though it's hidden */
     }
 
     input[type=number] {
         -moz-appearance: textfield;
         padding: 10px 0;
         text-align: center;
-        /* Firefox */
     }
 
     form {
         display: flex;
         justify-content: center;
+        flex-direction: column;
         align-items: center;
-        padding: 10px;
+        padding: 20px 10px;
         border: 2px solid gray;
         width: 60%;
+        margin: 10px 0;
+        background-color: rgb(0, 0, 0, 0.8);
     }
 
-    form>* {
+    .inputs {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .inputs>* {
         margin: 1rem 2rem;
     }
 
     .result {
-        width: 60%;
+        width: 100%;
         margin: 20px 0;
         text-align: center;
     }
 
+
+    .result>* {
+        padding: 10px 0;
+        border-top: 1px solid rgb(223, 119, 0);
+    }
+
+    .result>*:last-child {
+        padding-bottom: 0;
+    }
+
     select {
-        background-color: orange;
+        background-color: rgb(235, 70, 3);
         border: none;
         color: white;
         padding: 10px 0;
@@ -82,7 +105,7 @@ if (isset($_SESSION['clicked'])) {
     }
 
     #convert-btn {
-        background-color: orangered;
+        background-color: rgb(223, 119, 0);
         padding: 10px;
         border: none;
         cursor: pointer;
@@ -90,105 +113,132 @@ if (isset($_SESSION['clicked'])) {
         letter-spacing: 2px;
         font-weight: bold;
     }
+
+    @media only screen and (max-width: 1572px) {
+        form {
+            width: 90%;
+        }
+    }
+
+    @media only screen and (max-width: 1080px) {
+        .inputs {
+            flex-direction: column;
+        }
+    }
+
+    @media only screen and (max-width: 480px) {
+
+        form h1 {
+            font-size: 1.5rem;
+            text-align: center;
+        }
+
+        .inputs>input,
+        .inputs>select {
+            width: 100%;
+        }
+    }
     </style>
 </head>
 
 <body>
-    <h1>Convertisseur de monnaies</h1>
     <form action="index.php" method="POST">
-        <input type="number" name="money" step="any" placeholder="Monnaie locale" value=<?php if (isset($_POST['money'])) {
-                                                                                            echo $_POST['money'];
-                                                                                        } ?> />
-        <select name="devise1">
-            <option value="euro" <?php if ((isset($_POST['devise1']) && $_POST['devise1'] == "euro" && isset($_SESSION['clicked']))) {
+        <h1>Convertisseur de monnaies</h1>
+        <div class="inputs">
+            <input type="number" name="money" step="any" placeholder="Monnaie locale" required value=<?php if (isset($_POST['money'])) {
+                                                                                                            echo $_POST['money'];
+                                                                                                        } ?> />
+            <select name="devise1">
+                <option value="euro" <?php if ((isset($_POST['devise1']) && $_POST['devise1'] == "euro") || (isset($_GET['switched']) && $_GET['switched'] == 'false')) {
+                                            echo ' selected="selected"';
+                                        } ?>>Euro (EUR)</option>
+                <option value="dollar" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dollar" || (isset($_GET['switched']) && $_GET['switched'] == 'false')) {
+                                            echo ' selected="selected"';
+                                        } ?>>Dollar (USD)</option>
+                <option value="yen" <?php if ((isset($_POST['devise1']) && $_POST['devise1'] == "yen" || (isset($_GET['switched']) && $_GET['switched'] == 'false'))) {
                                         echo ' selected="selected"';
-                                    } ?>>Euro (EUR)</option>
-            <option value="dollar" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dollar") {
+                                    } ?>>
+                    Yen
+                    (JPY)</option>
+                <option value="livre" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "livre") {
+                                            echo ' selected="selected"';
+                                        } ?>>Livre (GBP)</option>
+                <option value="franc suisse" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "franc suisse") {
+                                                    echo ' selected="selected"';
+                                                } ?>>Franc suisse (CHF)</option>
+                <option value="dollar canadien" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dollar canadien") {
+                                                    echo ' selected="selected"';
+                                                } ?>>dollar canadien (CAD)</option>
+                <option value="dirham" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dirham") {
+                                            echo ' selected="selected"';
+                                        } ?>>Dirham (MAR)</option>
+                <option value="yuan" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "yuan") {
+                                            echo ' selected="selected"';
+                                        } ?>>Yuan (CNY)</option>
+                <option value="baht" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "baht") {
+                                            echo ' selected="selected"';
+                                        } ?>>Baht (THB)</option>
+                <option value="rouble russe" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "rouble russe") {
+                                                    echo ' selected="selected"';
+                                                } ?>>Rouble russe (RUB)</option>
+            </select>
+
+            <button name="switched"><i class="fa-solid fa-repeat"></i></button>
+            <select name="devise2">
+                <option value="euro" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "euro") {
+                                            echo ' selected="selected"';
+                                        } ?>>Euro (EUR)</option>
+                <option value="dollar" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dollar") {
+                                            echo ' selected="selected"';
+                                        } ?>>Dollar (USD)</option>
+                <option value="yen" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "yen") {
                                         echo ' selected="selected"';
-                                    } ?>>Dollar (USD)</option>
-            <option value="yen" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "yen") {
-                                    echo ' selected="selected"';
-                                } ?>>
-                Yen
-                (JPY)</option>
-            <option value="livre" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "livre") {
-                                        echo ' selected="selected"';
-                                    } ?>>Livre (GBP)</option>
-            <option value="franc suisse" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "franc suisse") {
-                                                echo ' selected="selected"';
-                                            } ?>>Franc suisse (CHF)</option>
-            <option value="dollar canadien" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dollar canadien") {
-                                                echo ' selected="selected"';
-                                            } ?>>dollar canadien (CAD)</option>
-            <option value="dirham" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "dirham") {
-                                        echo ' selected="selected"';
-                                    } ?>>Dirham (MAR)</option>
-            <option value="yuan" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "yuan") {
-                                        echo ' selected="selected"';
-                                    } ?>>Yuan (CNY)</option>
-            <option value="baht" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "baht") {
-                                        echo ' selected="selected"';
-                                    } ?>>Baht (THB)</option>
-            <option value="rouble russe" <?php if (isset($_POST['devise1']) && $_POST['devise1'] == "rouble russe") {
-                                                echo ' selected="selected"';
-                                            } ?>>Rouble russe (RUB)</option>
-        </select>
-        <button onclick=<?php header('index.php');
-                        if (isset($_SESSION['clicked'])) {
-                            if ($_SESSION['clicked'] == 'true') {
-                                $_SESSION['clicked'] = 'false';
-                            } else {
-                                $_SESSION['clicked'] = 'true';
-                            }
-                        } else {
-                            $_SESSION['clicked'] = 'true';
-                        }; ?>><i class="fa-solid fa-repeat"></i></button>
-        <select name="devise2">
-            <option value="euro" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "euro") {
-                                        echo ' selected="selected"';
-                                    } ?>>Euro (EUR)</option>
-            <option value="dollar" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dollar") {
-                                        echo ' selected="selected"';
-                                    } ?>>Dollar (USD)</option>
-            <option value="yen" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "yen") {
-                                    echo ' selected="selected"';
-                                } ?>>
-                Yen
-                (JPY)</option>
-            <option value="livre" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "livre") {
-                                        echo ' selected="selected"';
-                                    } ?>>Livre (GBP)</option>
-            <option value="franc suisse" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "franc suisse") {
-                                                echo ' selected="selected"';
-                                            } ?>>Franc suisse (CHF)</option>
-            <option value="dollar canadien" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dollar canadien") {
-                                                echo ' selected="selected"';
-                                            } ?>>dollar canadien (CAD)</option>
-            <option value="dirham" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dirham") {
-                                        echo ' selected="selected"';
-                                    } ?>>Dirham (MAR)</option>
-            <option value="yuan" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "yuan") {
-                                        echo ' selected="selected"';
-                                    } ?>>Yuan (CNY)</option>
-            <option value="baht" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "baht") {
-                                        echo ' selected="selected"';
-                                    } ?>>Baht (THB)</option>
-            <option value="rouble russe" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "rouble russe") {
-                                                echo ' selected="selected"';
-                                            } ?>>Rouble russe (RUB)</option>
-        </select>
-        <input id="convert-btn" type="submit" value="Convertir" />
+                                    } ?>>
+                    Yen
+                    (JPY)</option>
+                <option value="livre" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "livre") {
+                                            echo ' selected="selected"';
+                                        } ?>>Livre (GBP)</option>
+                <option value="franc suisse" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "franc suisse") {
+                                                    echo ' selected="selected"';
+                                                } ?>>Franc suisse (CHF)</option>
+                <option value="dollar canadien" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dollar canadien") {
+                                                    echo ' selected="selected"';
+                                                } ?>>dollar canadien (CAD)</option>
+                <option value="dirham" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "dirham") {
+                                            echo ' selected="selected"';
+                                        } ?>>Dirham (MAR)</option>
+                <option value="yuan" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "yuan") {
+                                            echo ' selected="selected"';
+                                        } ?>>Yuan (CNY)</option>
+                <option value="baht" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "baht") {
+                                            echo ' selected="selected"';
+                                        } ?>>Baht (THB)</option>
+                <option value="rouble russe" <?php if (isset($_POST['devise2']) && $_POST['devise2'] == "rouble russe") {
+                                                    echo ' selected="selected"';
+                                                } ?>>Rouble russe (RUB)</option>
+            </select>
+            <input id="convert-btn" type="submit" value="Convertir" />
+        </div>
+        <?php
+        if (isset($_POST['money']) && $_POST['money'] != '') {
+            if ($_POST['money'] > 0) {
+                if ($_POST['devise1'] != $_POST['devise2']) {
+                    $moneyToConvert = $devises[$_POST['devise1']][$_POST['devise2']] * $_POST['money'];
+                    echo "<div class='result'>
+                    <h1>" . $_POST['money'] . ' ' . $_POST['devise1'] . '(s)' . ' = ' . $moneyToConvert . ' ' . $_POST['devise2'] . '(s)' . "</h1>
+                    <h2>1 " . $_POST['devise1'] . " = " . $devises[$_POST['devise1']][$_POST['devise2']] . ' ' . $_POST['devise2'] . '(s)' . "</h2>
+                    <h2>1 " . $_POST['devise2'] . " = " . $devises[$_POST['devise2']][$_POST['devise1']] . ' ' . $_POST['devise1'] . '(s)' . "</h2>
+                    </div>";
+                } else {
+                    echo '<h1>Sélectionnez 2 devises différentes !</h1>';
+                }
+            } else {
+                echo '<h1>Entrez un nombre strictement positif !</h1>';
+            }
+        }
+        ?>
     </form>
-    <?php
-    if (isset($_POST['money']) && $_POST['money'] != '') {
-        $moneyToConvert = $devises[$_POST['devise1']][$_POST['devise2']] * $_POST['money'];
-        echo "<div class='result'>
-                <h1>" . $_POST['money'] . ' ' . $_POST['devise1'] . '(s)' . ' = ' . $moneyToConvert . ' ' . $_POST['devise2'] . '(s)' . "</h1>
-                <h2>1 " . $_POST['devise1'] . " = " . $devises[$_POST['devise1']][$_POST['devise2']] . ' ' . $_POST['devise2'] . '(s)' . "</h2>
-                <h2>1 " . $_POST['devise2'] . " = " . $devises[$_POST['devise2']][$_POST['devise1']] . ' ' . $_POST['devise1'] . '(s)' . "</h2>
-            </div>";
-    }
-    ?>
 </body>
 
 </html>
